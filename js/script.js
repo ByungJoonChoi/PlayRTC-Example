ce7ec20aeebb4954ece3f7f30cdd3c9bbe52a087
Peter Choi <cbj0618@gmail.com>
2018-01-16 14:15:58 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ var database = firebase.database().ref();
 var yourVideo = document.getElementById("yourVideo");
 var friendsVideo = document.getElementById("friendsVideo");
 var yourId = Math.floor(Math.random()*1000000000);
+var btnCall = document.getElementById("btnCall");
 //Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
 var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'}]};
 var pc = new RTCPeerConnection(servers);
@@ -54,6 +55,8 @@ function showFriendsFace() {
     .then(offer => pc.setLocalDescription(offer) )
     .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
 }
+  btnCall.addEventListener("click", showFriendsFace);
+  
   showMyFace();
 });
 
