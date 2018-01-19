1ba173733c31c6382ef770748a58c4d090098fe8
Peter Choi <cbj0618@gmail.com>
2018-01-19 20:49:46 +0900

    소스코드 리팩토링

--- js/script.js
+++ js/script.js
@@ -1,66 +1,67 @@
 //Create an account on Firebase, and use the credentials they give you in place of the following
 document.addEventListener("DOMContentLoaded", function(){
-  var config = {
- apiKey: "AIzaSyAewTR7oYq4Yx_8ny59p7prqC0tovB7wLY",
- authDomain: "rtcdemian.firebaseapp.com",
- databaseURL: "https://rtcdemian.firebaseio.com",
- projectId: "rtcdemian",
- storageBucket: "rtcdemian.appspot.com",
- messagingSenderId: "542134912696"
-};
-firebase.initializeApp(config);
+  let config = {
+   apiKey: "AIzaSyAewTR7oYq4Yx_8ny59p7prqC0tovB7wLY",
+   authDomain: "rtcdemian.firebaseapp.com",
+   databaseURL: "https://rtcdemian.firebaseio.com",
+   projectId: "rtcdemian",
+   storageBucket: "rtcdemian.appspot.com",
+   messagingSenderId: "542134912696"
+  };
+  firebase.initializeApp(config);
 
-var database = firebase.database().ref();
-var yourVideo = document.getElementById("yourVideo");
-var friendsVideo = document.getElementById("friendsVideo");
-var yourId = Math.floor(Math.random()*1000000000);
-var btnCall = document.getElementById("btnCall");
-//Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
-var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'}]};
-var pc = new RTCPeerConnection(servers);
-pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
-pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+  let database = firebase.database().ref();
+  let yourVideo = document.getElementById("yourVideo");
+  let friendsVideo = document.getElementById("friendsVideo");
+  let yourId = Math.floor(Math.random()*1000000000); // TODO: 사용자 ID로 바꾸어야 함.
+  let btnCall = document.getElementById("btnCall");
 
-function sendMessage(senderId, data) {
-    var msg = database.push({ sender: senderId, message: data });
-    msg.remove();
-}
+  //Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
+  let servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'}]};
 
-function readMessage(data) {
-    var msg = JSON.parse(data.val().message);
-    var sender = data.val().sender;
-    if (sender != yourId) {
-        if (msg.ice != undefined)
-            pc.addIceCandidate(new RTCIceCandidate(msg.ice));
-        else if (msg.sdp.type == "offer")
-            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
-              .then(() => pc.createAnswer())
-              .then(answer => pc.setLocalDescription(answer))
-              .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
-        else if (msg.sdp.type == "answer")
-            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
-    }
-};
+  let pc = new RTCPeerConnection(servers);
+  pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+  pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
 
-database.on('child_added', readMessage);
+  function sendMessage(senderId, data) {
+      let msg = database.push({ sender: senderId, message: data });
+      msg.remove();
+  }
 
-function showMyFace() {
-  navigator.mediaDevices.getUserMedia({audio:true, video:true})
-    .then(stream => yourVideo.srcObject = stream)
-    .then(stream => pc.addStream(stream));
-}
+  function readMessage(data) {
+      let msg = JSON.parse(data.val().message);
+      let sender = data.val().sender;
+      if (sender != yourId) {
+          if (msg.ice != undefined)
+              pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+          else if (msg.sdp.type == "offer")
+              pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+                .then(() => pc.createAnswer())
+                .then(answer => pc.setLocalDescription(answer))
+                .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
+          else if (msg.sdp.type == "answer")
+              pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+      }
+  };
 
-function showFriendsFace() {
-  pc.createOffer()
-    .then(offer => pc.setLocalDescription(offer) )
-    .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
-}
-  
-btnCall.addEventListener("click", function(){
-  console.log("click");
-  showFriendsFace();
+  database.on('child_added', readMessage);
+
+  function showMyFace() {
+    navigator.mediaDevices.getUserMedia({audio:true, video:true})
+      .then(stream => yourVideo.srcObject = stream)
+      .then(stream => pc.addStream(stream));
+  }
+
+  function showFriendsFace() {
+    pc.createOffer()
+      .then(offer => pc.setLocalDescription(offer) )
+      .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
+  }
+
+  btnCall.addEventListener("click", function(){
+    console.log("call button clicked!!");
+    showFriendsFace();
+  });
+
+    showMyFace();
 });
-  
-  showMyFace();
-});
-
